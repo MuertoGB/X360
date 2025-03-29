d9cfa8d72fd812e78c71b190748f5dbb677bd567
David Roberts <qbug@tuta.io>
2025-03-29 23:31:35 +0000

    Start refactor

--- X360/X360/Resources.cs
+++ X360/X360/Resources.cs
@@ -1,35 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace X360
 {
-    /// <summary>
-    /// Resource data
-    /// </summary>
     public static class PublicResources
     {
         /// <summary>
         /// Dash Image
         /// </summary>
-        public static Image DashImage { get { return global::X360.Properties.Resources.Dash; }}
+        public static Image DashImage => Properties.Resources.Dash;
+
         /// <summary>
         /// No Achievement image
         /// </summary>
-        public static Image NoImage { get { return global::X360.Properties.Resources.NoImg; }}
+        public static Image NoImage => Properties.Resources.NoImg;
+
         /// <summary>
         /// Achievement locked image
         /// </summary>
-        public static Image Locked { get { return global::X360.Properties.Resources.Locked; }}
+        public static Image Locked => Properties.Resources.Locked;
+
         /// <summary>
         /// Paw icon
         /// </summary>
-        public static Icon PawIcon { get { return global::X360.Properties.Resources.paw; }}
+        public static Icon PawIcon => Properties.Resources.paw;
+
         /// <summary>
         /// GPL License
         /// </summary>
-        public static string GPL { get { return global::X360.Properties.Resources.GPL30; }}
+        public static string GPL => Properties.Resources.GPL30;
     }
 }
